fe21c2ff76bd5875ec4fc7df7698335e576e8814
pruthvipatnala <pruthvipatnala@gmail.com>
2019-06-28 19:11:09 +0530

    Added comments

--- transfer_code.py
+++ transfer_code.py
@@ -1,26 +1,25 @@
 import subprocess
 from os import listdir
-from os.path import isfile, join
 
 
 def transfer():
-	#print("Enter destination IP address : ")
+	"""
+	Function to transfer file from local system to AWS EC2 instance.
+	"""
 	ip = input("Enter destination IP address : ")
 	user = input("Enter User name : ")
+	pemFileName = input("Enter pem file name : ")
 	ipMod = ip.replace('.',"-")
-	#cmd = "scp -r -i /transfer "+user+"@ec2-"+ipMod+".compute-1.amazonaws.com: /"
-	#args = ["scp", "-i", "crio.pem", "-r","transfer", user+"@ec2-"+ipMod+".compute-1.amazonaws.com:", "/"]
-	#args = ["ls", "/transfer"]
-	#send = subprocess.Popen(args)
-	#output = send.communicate()
-	#onlyfiles = [f for f in listdir("/transfer") if isfile(join("/transfer", f))]
+
+	#List of files in the transfer folder
 	onlyfiles = [f for f in listdir("transfer")]
-	#print(onlyfiles)
 	for file in onlyfiles:
-		args = ["sudo", "scp", "-i", "crio.pem","transfer/"+file, user+"@ec2-"+ipMod+".compute-1.amazonaws.com:~/"]
+		args = ["sudo", "scp", "-i", pemFileName,"transfer/"+file, user+"@ec2-"+ipMod+".compute-1.amazonaws.com:~/"]
+		#scp command to transfer file to server
 		print(" ".join(args))
 		send = subprocess.Popen(args, stdout=subprocess.PIPE)
 		output = send.communicate()
-		print(output)
+		print(output[0])
+
 if __name__ == '__main__':
 	transfer()
